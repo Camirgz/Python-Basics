9b9c2f8a650be4577e5946a5fa84e2b89d2ac9f2
Camirgz <77652366+Camirgz@users.noreply.github.com>
2021-03-26 22:34:01 -0600

    Update Triangle area calculator.py

--- Triangle area calculator.py
+++ Triangle area calculator.py
@@ -2,3 +2,4 @@ print("Welcome to the triangle and perimeter calculator")
 base=input("Insert how long is the base of the triangle: ")
 height=input("Insert how long is the height of the triangle: ")
 calculus=(int(base*height):
+print(str(calculus))
