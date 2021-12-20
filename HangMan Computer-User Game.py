90f98b674432abf58f660bafc616baf6cb1e90a4
Camirgz <77652366+Camirgz@users.noreply.github.com>
2021-12-19 18:52:46 -0700

    HangMan TXT Interactive Game
    This HangMan game works with the Random Words via choosing any random word and making the user guess it!

---
+++ HangMan Computer-User Game.py
@@ -0,0 +1,33 @@
+import random
+with open(r"C:\Users\Camila\Documents\RandomWords.txt" , "r") as f:
+    words = f.readlines()
+
+word = random.choice(words)[:-1]
+
+allowed_errors = 15
+guesess = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesess:
+            print(letter, end="")
+        else:
+            print("_", end="")
+    print("")
+
+    guess = input(f"Allowed Errors Left {allowed_errors}, Next guess: ")
+    guesess.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -= 1
+        if allowed_errors == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesess:
+            done = False
+if done:
+    print(f"You found the word! It was {word}")
+else:
+    print(f"Game over! It was {word}")
