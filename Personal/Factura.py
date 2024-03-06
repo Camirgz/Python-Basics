4db80498fe6e630046a587559ac5a994912d64fa
iZackk__26 <128321469+iZackk26@users.noreply.github.com>
2024-03-05 22:05:16 -0600

    Update Factura.py
    feat: Fixing some unnecessary variables, and distributing the code into segments

--- Personal/Factura.py
+++ Personal/Factura.py
@@ -1,21 +1,15 @@
-lista_sumada=[]
-total=0
-fecha=input("Digite la fecha del día de hoy: ")
-nombre=input("Digite su nombre: ")
-while True:
-    producto=input("Ingrese el nombre de su producto: ")
-    precio=input("Ingrese el costo de su producto: ")
-    total+=int(precio)
-    lista_sumada.append(producto + " " + precio)
-    continuar=input("Si desea continuar digite x: ")
-    if continuar != "x":
-        lista_linda_sumada=str("\n".join(lista_sumada))
-        print("- - - - - - - - - - - - - - - - - - - - - - - ")
-        print(nombre)
-        print(str(fecha))
-        print(lista_linda_sumada)
-        print("El total es de: " + str(total))
-        print("La cantidad de elementos que compró es de " +str(len(lista_sumada)))
-        print("- - - - - - - - - - - - - - - - - - - - - - - ")
-        break
+def total_productos() -> dict:
+    dic_productos ={}
+    producto = input("Ingrese le nombre de su producto")
+    precio = int(input("Ingrese el costo de su producto"))
+    dic_productos[f'{producto}'] = precio
+    return dic_productos
 
+def imprimir_productos(dic_productos) -> None:
+    sum_total = 0
+    print("- - - - - - - - - - - - - - - - - - - - - - - ")
+    for clave, valor in dic_productos.keys():
+        print(f"Nombre: {clave}, Precio: {valor}")
+        sum_total += valor
+    
+    print(f"El precio total de los articulos es: {sum_total}")
